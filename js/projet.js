3692cebcf9f5594cd3023504ddfc5478196ac313
JeanneThibert <jeannedevelop25@gmail.com>
2019-03-27 10:33:29 +0000

    [MEDIA , WOO.JS]Ajout des medias pour la partie projet et ajout des sections de la partie projet

---
+++ js/projet.js
@@ -0,0 +1,31 @@
+<div class=" col-12 col-sm-12 col-md-12 col-lg-12 pt-3" style="float:none;">
+<a class="portfolio-link" data-toggle="modal" href="#Modal1">
+<div class="wow zoomIn opacity">
+  <img class="img-fluid" src="../media/access_movie.jpg" alt="lalaallala" >
+</div>
+</a>
+</div>
+
+<div class=" col-12 col-sm-12 col-md-12 col-lg-12 pt-3" style="float:none;">
+<a class="portfolio-link" data-toggle="modal" href="#Modal2">
+  <div class="wow zoomIn opacity">
+  <img class="img-fluid" src="../media/avatar.jpg" alt="lalaallala" >
+</div>
+</a>
+</div>
+
+<div class=" col-12 col-sm-12 col-md-12 col-lg-12 pt-3" style="float:none;">
+<a class="portfolio-link" data-toggle="modal" href="#Modal3">
+  <div class="wow zoomIn opacity">
+  <img class="img-fluid" src="../media/puissance4.jpg" alt="lalaallala" >
+</div>
+</a>
+</div>
+
+<div class=" col-12 col-sm-12 col-md-12 col-lg-12 pt-3" style="float:none;">
+<a class="portfolio-link" data-toggle="modal" href="#Modal4">
+  <div class="wow zoomIn opacity">
+  <img class="img-fluid" src="../media/explorateur.jpg" alt="lalaallala" >
+</div>
+</a>
+</div>
